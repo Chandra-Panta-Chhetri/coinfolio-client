f0a6499247ea78dde0869669ce6b4deff47d6cc9
Chandra Panta C <chandra.panta345@hotmail.com>
2021-05-11 21:50:47 -0400

    Adds home screen icon shortcuts.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,19 @@
 import React, { useEffect } from "react";
-import { StyleSheet, View, ScrollView } from "react-native";
+import { StyleSheet, View, ScrollView, TouchableOpacity } from "react-native";
 import { connect } from "react-redux";
 import { selectNotifications } from "../redux/notification/notification.selectors";
 import { createStructuredSelector } from "reselect";
 import { addSuccessNotification } from "../redux/notification/notification.actions";
-import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
+import {
+  Card,
+  Paragraph,
+  IconButton,
+  Caption,
+  TouchableRipple
+} from "react-native-paper";
 
-function HomeScreen({ navigation, notifications, test }) {
-  useEffect(() => {
-    test();
-  }, []);
+function HomeScreen({ navigation }) {
+  useEffect(() => {}, []);
 
   return (
     <View style={styles.container}>
@@ -35,19 +39,76 @@ function HomeScreen({ navigation, notifications, test }) {
           </ScrollView>
         </Card.Content>
       </Card>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.shortcutIconContainer}
+      >
+        <TouchableRipple rippleColor="rgba(0, 0, 0, .32)" onPress={() => {}}>
+          <View style={styles.shortcutIconItem}>
+            <View style={styles.iconButton}>
+              <IconButton icon="bell-alert" color="blue" />
+            </View>
+            <Caption style={styles.iconButtonLabel}>Price Alerts</Caption>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple rippleColor="rgba(0, 0, 0, .32)" onPress={() => {}}>
+          <View style={styles.shortcutIconItem}>
+            <View style={styles.iconButton}>
+              <IconButton icon="compare" color="blue" />
+            </View>
+            <Caption style={styles.iconButtonLabel}>Compare</Caption>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple rippleColor="rgba(0, 0, 0, .32)" onPress={() => {}}>
+          <View style={styles.shortcutIconItem}>
+            <View style={styles.iconButton}>
+              <IconButton icon="calculator" color="blue" />
+            </View>
+            <Caption style={styles.iconButtonLabel}>Converter</Caption>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple rippleColor="rgba(0, 0, 0, .32)" onPress={() => {}}>
+          <View style={styles.shortcutIconItem}>
+            <View style={styles.iconButton}>
+              <IconButton icon="eye" color="blue" />
+            </View>
+            <Caption style={styles.iconButtonLabel}>Watchlist</Caption>
+          </View>
+        </TouchableRipple>
+      </ScrollView>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     padding: 10
   },
   rightMargin: {
     marginRight: 10
   },
   bold: {
+    fontWeight: "bold"
+  },
+  shortcutIconContainer: {
+    justifyContent: "space-between",
+    flex: 1,
+    marginTop: 10
+  },
+  shortcutIconItem: {
+    alignItems: "center",
+    padding: 5
+  },
+  iconButton: {
+    borderWidth: 5,
+    borderRadius: 30,
+    borderColor: "white",
+    backgroundColor: "#ECECEC"
+  },
+  iconButtonLabel: {
+    color: "black",
     fontWeight: "bold"
   }
 });
