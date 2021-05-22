98629e14c154baf5610f7fc402a7d3751caf006d
Chandra Panta C <chandra.panta345@hotmail.com>
2021-05-22 11:39:35 -0400

    Adds ripple effect on bottom tabs for Android to improve native UX.

--- src/navigation/BottomTabNavigation.js
+++ src/navigation/BottomTabNavigation.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Platform } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { AntDesign } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
@@ -8,35 +9,45 @@ import MarketScreen from "../screens/MarketScreen";
 import SettingsScreen from "../screens/SettingsScreen";
 import NewsScreen from "../screens/NewsScreen";
 import PortfolioScreen from "../screens/PortfolioScreen";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { useTheme } from "react-native-paper";
 
-const BottomTab = createBottomTabNavigator();
+const BottomTab =
+  Platform.OS === "android"
+    ? createMaterialBottomTabNavigator()
+    : createBottomTabNavigator();
 
-const HomeTabIcon = ({ color, size }) => (
+const HomeTabIcon = ({ color, size = 21.5 }) => (
   <AntDesign name="home" size={size} color={color} />
 );
 
-const MarketTabIcon = ({ color, size }) => (
+const MarketTabIcon = ({ color, size = 21.5 }) => (
   <AntDesign name="barschart" size={size} color={color} />
 );
 
-const PortfolioTabIcon = ({ color, size }) => (
+const PortfolioTabIcon = ({ color, size = 21.5 }) => (
   <Feather name="pie-chart" size={size} color={color} />
 );
 
-const NewsTabIcon = ({ color, size }) => (
+const NewsTabIcon = ({ color, size = 21.5 }) => (
   <FontAwesome name="newspaper-o" size={size} color={color} />
 );
 
-const SettingsTabIcon = ({ color, size }) => (
+const SettingsTabIcon = ({ color, size = 21.5 }) => (
   <AntDesign name="setting" size={size} color={color} />
 );
 
 const BottomTabNavigation = () => {
+  const { colors } = useTheme();
+
   return (
     <BottomTab.Navigator
       tabBarOptions={{
         showLabel: false
       }}
+      labeled={false}
+      activeColor={colors.primary}
+      barStyle={{ backgroundColor: colors.card }}
     >
       <BottomTab.Screen
         name="Home"
