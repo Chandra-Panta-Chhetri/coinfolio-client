72192cfcf7c758f796d4865339b5a3f35ae1e1c9
Chandra Panta C <chandra.panta345@hotmail.com>
2021-05-30 18:06:57 -0400

    Designs portfolio screen for when user is not logged in.

--- src/screens/PortfolioScreen.js
+++ src/screens/PortfolioScreen.js
@@ -1,23 +1,46 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Image } from "react-native";
 import { connect } from "react-redux";
 import { selectCurrentUser } from "../redux/user/user.selectors";
 import { createStructuredSelector } from "reselect";
-import { Button, Text } from "react-native-paper";
+import { Button, Text, Headline, Subheading } from "react-native-paper";
+
+const UnAuthenticatedPortfolio = ({ navigation }) => (
+  <View style={styles.container}>
+    <Image
+      source={{
+        uri: "https://www.pngkit.com/png/detail/243-2438344_3d-chart-4-icon-png-3-pie-chart.png"
+      }}
+      style={styles.noAuthImage}
+    />
+    <Headline style={styles.noAuthHeading}>
+      Track Your Crypto Portfolio
+    </Headline>
+    <Subheading style={styles.noAuthSubheading}>
+      Signup or login to track your crypto transactions, current balance and
+      profit/loss.
+    </Subheading>
+    <Button
+      uppercase={false}
+      onPress={() => navigation.navigate("SignUp")}
+      style={styles.signUpBtn}
+      mode="contained"
+    >
+      Create an account
+    </Button>
+    <Button
+      onPress={() => navigation.navigate("Login")}
+      mode="contained"
+      uppercase={false}
+    >
+      Login
+    </Button>
+  </View>
+);
 
 function PortfolioScreen({ navigation, isAuthenticated }) {
   if (!isAuthenticated) {
-    return (
-      <View style={styles.container}>
-        <View>
-          <Text style={styles.text}>Portfolio Screen</Text>
-        </View>
-        <Button onPress={() => navigation.navigate("SignUp")}>
-          Create an account
-        </Button>
-        <Button onPress={() => navigation.navigate("Login")}>Login</Button>
-      </View>
-    );
+    return <UnAuthenticatedPortfolio navigation={navigation} />;
   }
 }
 
@@ -25,12 +48,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    padding: 10
   },
-  text: {
-    color: "#101010",
-    fontSize: 24,
-    fontWeight: "bold"
+  noAuthHeading: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 22,
+    letterSpacing: 2
+  },
+  noAuthSubheading: {
+    textAlign: "center",
+    letterSpacing: 1
+  },
+  signUpBtn: {
+    marginVertical: 10
+  },
+  noAuthImage: {
+    width: "100%",
+    height: 200,
+    backgroundColor: "transparent"
   }
 });
 
