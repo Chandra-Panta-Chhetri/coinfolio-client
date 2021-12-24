e09063d644b7a6a2c6acb05b1a0e2cfe29bb9ee6
Chandra Panta C <chandra.panta345@hotmail.com>
2021-12-23 22:32:12 -0500

    Changes navigation bar for android based on theme.

--- src/core/components/Main.js
+++ src/core/components/Main.js
@@ -2,7 +2,7 @@ import React from "react";
 import RootNavigator from "../navigators/Root";
 import { Provider as PaperProvider } from "react-native-paper";
 import { THEME } from "../../styles";
-import { SafeAreaView } from "react-native";
+import { Platform, SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { connect } from "react-redux";
 import { selectIsThemeDark } from "../../redux/preferences/preferences.selectors";
@@ -10,10 +10,15 @@ import NotificationSnackbar from "./NotificationSnackbar";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
+import * as NavigationBar from "expo-navigation-bar";
 
 const Main = ({ isThemeDark }) => {
   const theme = isThemeDark ? THEME.DARK : THEME.LIGHT;
   const containerStyles = { flex: 1, backgroundColor: theme.colors.card };
+
+  if (Platform.OS === "android") {
+    NavigationBar.setBackgroundColorAsync(theme.colors.card);
+  }
 
   return (
     <PaperProvider theme={theme}>
