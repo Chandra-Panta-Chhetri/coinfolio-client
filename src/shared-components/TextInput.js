1037cf855d30bf4c00785b847730ff0d2ba320a4
Chandra Panta C <chandra.panta345@hotmail.com>
2022-06-16 09:31:59 -0400

    Fixes bug where text input unreadable when highlighted in dark mode.

--- src/shared-components/TextInput.js
+++ src/shared-components/TextInput.js
@@ -4,7 +4,7 @@ import { TextInput, useTheme } from "react-native-paper";
 const CustomTextInput = (props) => {
   const { colors } = useTheme();
 
-  return <TextInput {...props} mode="outlined" activeOutlineColor={colors.text} />;
+  return <TextInput {...props} mode="outlined" activeOutlineColor={colors.text} selectionColor={colors.primary} />;
 };
 
 CustomTextInput.Icon = TextInput.Icon;
