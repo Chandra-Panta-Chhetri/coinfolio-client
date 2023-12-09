3ef1aff4270370d9f56ff560bba889d7ee08237d
Chandra Panta C <chandra.panta345@hotmail.com>
2023-12-09 12:02:47 -0500

    Removes custom bottom tab bar for android as it looks same as ios.

--- src/navigators/BottomTabNavigator.js
+++ src/navigators/BottomTabNavigator.js
@@ -1,64 +1,22 @@
 import React from "react";
-import { Platform } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeScreen, MarketOverviewScreen, SettingsScreen, DiscoverScreen } from "../screens";
-import { useTheme, BottomNavigation } from "react-native-paper";
+import { useTheme } from "react-native-paper";
 import { connect } from "react-redux";
 import { selectInitialScreen } from "../redux/preferences";
 import PortfolioNavigator from "./PortfolioNavigator";
-import { DEVICE_TYPES } from "../constants";
+import { GLOBAL_CONSTANTS } from "../constants";
 import SCREEN_NAMES from "./screen-names";
 import { DiscoverTabIcon, HomeTabIcon, MarketTabIcon, PortfolioTabIcon, SettingsTabIcon } from "./BottomTabIcons";
 
 const BottomTab = createBottomTabNavigator();
 
-const AndroidBottomTabBar = ({ navigation, state, descriptors, insets, theme, tabBarStyle }) => {
-  return (
-    <BottomNavigation.Bar
-      navigationState={state}
-      safeAreaInsets={insets}
-      onTabPress={({ route, preventDefault }) => {
-        const event = navigation.emit({
-          type: "tabPress",
-          target: route.key,
-          canPreventDefault: true
-        });
-
-        if (event.defaultPrevented) {
-          preventDefault();
-        } else {
-          navigation.navigate(route);
-        }
-      }}
-      renderIcon={({ route, focused, color }) => {
-        const { options } = descriptors[route.key];
-        if (options.tabBarIcon) {
-          return options.tabBarIcon({ focused, color });
-        }
-
-        return null;
-      }}
-      getLabelText={({ route }) => {
-        const { options } = descriptors[route.key];
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-            ? options.title
-            : route.title;
-
-        return label;
-      }}
-      activeColor={theme?.colors?.primary}
-      style={tabBarStyle}
-      shifting={false}
-    />
-  );
-};
-
 const BottomTabNavigator = ({ initialScreen }) => {
   const theme = useTheme();
   const tabBarStyle = { backgroundColor: theme?.colors?.card };
+  const tabBarItemStyle = {
+    marginTop: GLOBAL_CONSTANTS.SM_MARGIN
+  };
 
   return (
     <BottomTab.Navigator
@@ -66,15 +24,11 @@ const BottomTabNavigator = ({ initialScreen }) => {
         tabBarStyle,
         tabBarShowLabel: true,
         headerShown: false,
-        tabBarActiveTintColor: theme?.colors?.primary
+        tabBarActiveTintColor: theme?.colors?.primary,
+        tabBarItemStyle
       }}
       initialRouteName={initialScreen?.value}
       backBehavior="none"
-      tabBar={
-        Platform.OS === DEVICE_TYPES.ANDROID
-          ? (props) => <AndroidBottomTabBar {...props} theme={theme} tabBarStyle={tabBarStyle} />
-          : undefined
-      }
     >
       <BottomTab.Screen
         name={SCREEN_NAMES.HOME}
